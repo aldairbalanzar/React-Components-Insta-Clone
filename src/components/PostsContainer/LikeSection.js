ab630c7a72d0cd59c7956f569d0818a405c81b7f
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-02-18 19:15:44 -0500

    updated files to mvp

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -3,7 +3,7 @@
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
 import React from "react";
 
-const LikeSection = props => {
+const LikeSection = ({likes, addLikes}) => {
   return (
     <div>
     <div
@@ -11,14 +11,14 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i className="far fa-heart" onClick={addLikes} />
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
     <p className="like-number">
-      
+      {likes}
       likes</p>
 </div>
   )
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dumdata from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+  const [data] = useState(dumdata);
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(post => (
+        <Post key={post.id} post={post}/> 
+      ))}
     </div>
   );
 };
